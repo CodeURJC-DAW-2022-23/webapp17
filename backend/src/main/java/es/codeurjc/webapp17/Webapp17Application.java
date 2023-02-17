788858dd60345ba6f645b4ad5ee156da5cc8c957
rattata2me <jorgevegarias1@gmail.com>
2023-02-17 19:45:59 +0100

    Created Spring Boot project and added Maven files

---
+++ backend/src/main/java/es/codeurjc/webapp17/Webapp17Application.java
@@ -0,0 +1,13 @@
+package es.codeurjc.webapp17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webapp17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webapp17Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/es/codeurjc/webapp17/Webapp17ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.codeurjc.webapp17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webapp17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
