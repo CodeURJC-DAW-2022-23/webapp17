d5c548568dbe87e19b6f5078941522a29170ce33
BiggestGuille <guigrsanti@gmail.com>
2023-02-22 11:52:55 +0100

    Navigation with Spring Controller updated

--- backend/src/main/java/es/codeurjc/webapp17/controller/HelloController.java
+++ backend/src/main/java/es/codeurjc/webapp17/controller/HelloController.java
@@ -17,9 +17,58 @@ public class HelloController {
 
     @GetMapping("/")
     @NeedsSecurity(role=Tools.Role.NONE)
-    public String handle(Model model) {
+    public String index(Model model) {
         model.addAttribute("message", users_service.getUsers().findByEmail("helloworld@email.com").get(0).getName());
         return "index";
+    }
+
+    @GetMapping("/info")
+    @NeedsSecurity(role=Tools.Role.NONE)
+    public String info(Model model) {
+        //model.addAttribute("message", users_service.getUsers().findByEmail("helloworld@email.com").get(0).getName());
+        return "info/info";
+    }
+
+    @GetMapping("/user")
+    @NeedsSecurity(role=Tools.Role.NONE)
+    public String user(Model model) {
+        //model.addAttribute("message", users_service.getUsers().findByEmail("helloworld@email.com").get(0).getName());
+        return "info/user";
+    }
+
+    @GetMapping("/login")
+    @NeedsSecurity(role=Tools.Role.NONE)
+    public String login(Model model) {
+        //model.addAttribute("message", users_service.getUsers().findByEmail("helloworld@email.com").get(0).getName());
+        return "login/login";
+    }
+
+    @GetMapping("/register")
+    @NeedsSecurity(role=Tools.Role.NONE)
+    public String register(Model model) {
+        //model.addAttribute("message", users_service.getUsers().findByEmail("helloworld@email.com").get(0).getName());
+        return "login/register";
+    }
+
+    @GetMapping("/menu")
+    @NeedsSecurity(role=Tools.Role.NONE)
+    public String menu(Model model) {
+        //model.addAttribute("message", users_service.getUsers().findByEmail("helloworld@email.com").get(0).getName());
+        return "menu/menu";
+    }
+
+    @GetMapping("/order")
+    @NeedsSecurity(role=Tools.Role.NONE)
+    public String order(Model model) {
+        //model.addAttribute("message", users_service.getUsers().findByEmail("helloworld@email.com").get(0).getName());
+        return "dishes/order";
+    }
+
+    @GetMapping("/book")
+    @NeedsSecurity(role=Tools.Role.NONE)
+    public String book(Model model) {
+        //model.addAttribute("message", users_service.getUsers().findByEmail("helloworld@email.com").get(0).getName());
+        return "dishes/book";
     }
 
 }
