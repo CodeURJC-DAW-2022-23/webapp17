ff2e8af5dde03791557aca1bee5a4127e0a84760
BiggestGuille <guigrsanti@gmail.com>
2023-03-22 12:59:17 +0100

    CartApiController creation

---
+++ backend/src/main/java/es/codeurjc/webapp17/controller/api/CartApiController.java
@@ -0,0 +1,33 @@
+package es.codeurjc.webapp17.controller.api;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import es.codeurjc.webapp17.model.Comment;
+import es.codeurjc.webapp17.service.CommentsService;
+import es.codeurjc.webapp17.tools.NeedsSecurity;
+import es.codeurjc.webapp17.tools.Tools;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.media.Content;
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping(Tools.API_HEADER + "/carts/")
+public class CartApiController {
+
+
+    
+}
