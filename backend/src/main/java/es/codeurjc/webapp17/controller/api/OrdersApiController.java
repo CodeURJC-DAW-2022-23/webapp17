13298a24bde814f5dfdbec19bde65a21767bf42f
Jesus <jesus@debian-BULLSEYE-live-builder-AMD64>
2023-03-23 11:20:49 +0100

    More changes to API orders and admin

--- backend/src/main/java/es/codeurjc/webapp17/controller/api/OrdersApiController.java
+++ backend/src/main/java/es/codeurjc/webapp17/controller/api/OrdersApiController.java
@@ -1,11 +1,15 @@
 package es.codeurjc.webapp17.controller.api;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+import es.codeurjc.webapp17.model.Cart;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +17,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.ui.Model;
+import es.codeurjc.webapp17.repository.CartsRepo;
 import es.codeurjc.webapp17.model.Comment;
 import es.codeurjc.webapp17.service.CommentsService;
 import es.codeurjc.webapp17.tools.NeedsSecurity;
@@ -23,10 +28,14 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.media.Content;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.data.domain.Page;
 
 @RestController
 @RequestMapping(Tools.API_HEADER + "/orders/")
 public class OrdersApiController {
+
+        @Autowired
+        CartsRepo cartsRepo;
 
     @GetMapping("/viewOrders")
     @Operation(summary = "View orders")
@@ -48,8 +57,19 @@ public class OrdersApiController {
             )
     })
     @NeedsSecurity(role=Tools.Role.ADMIN)
-    public void viewOrdersPaginated(){
-
+    public String viewOrdersPaginated(Model model, HttpServletRequest request, @RequestParam(defaultValue = "0") int page){
+         int pageSize = 8;
+        Page<Cart> carts = cartsRepo.findByStatusNot(Cart.STATUS_NEW, PageRequest.of(page, pageSize));
+        model.addAttribute("prevPag", (int)Math.max(0, page-1));
+        int num = (int)Math.ceil((float)carts.getSize() / (float)pageSize);
+        model.addAttribute("nextPag", (int)Math.min(page+1, num-1));
+        
+        
+        if(!carts.isEmpty()){
+            model.addAttribute("hasCarts", true);
+            model.addAttribute("orders", carts);
+        }
+        return "admin/orders";
     };
 
 
@@ -73,6 +93,14 @@ public class OrdersApiController {
             )
     })
     @NeedsSecurity(role=Tools.Role.ADMIN)
-    public void changeState(){
-    };
+    public  ResponseEntity changeState(Model model, @RequestParam(name = "id") Long id,@RequestParam(name = "action") int action, HttpServletRequest request) {
+        if(action==1){
+                cartsRepo.deleteById(id);
+            }else{
+                Cart c = cartsRepo.findById(id).get();
+                c.setStatus(Cart.STATUS_DONE);
+                cartsRepo.saveAndFlush(c);
+            }
+            return ResponseEntity.status(HttpStatus.SEE_OTHER).location(URI.create("/adminOrders")).build();
+};
 }
