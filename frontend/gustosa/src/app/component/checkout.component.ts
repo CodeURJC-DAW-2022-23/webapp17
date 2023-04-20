f406484748ca6bf00913d694c2c0fd80c2c6a257
BiggestGuille <guigrsanti@gmail.com>
2023-04-20 18:21:34 +0200

    Checkout form fully functional

---
+++ frontend/gustosa/src/app/component/checkout.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { CartService } from '../service/cart.service';
+import { Observable, catchError, map, of } from 'rxjs';
+import { SessionService } from '../service/session.service';
+
+@Component({
+  selector: 'checkout',
+  templateUrl: './checkout.component.html',
+  styleUrls: []
+})
+
+export class CheckoutComponent {
+
+  checkout: Observable<any>;
+    
+  constructor(private cartService : CartService, private sessionService : SessionService){
+      this.checkout = cartService.getCart();
+      this.checkout.subscribe((data) => {
+      });
+  }
+
+  verifyFields(): boolean {
+    const requiredFields = document.querySelectorAll('[required]') as NodeListOf<HTMLInputElement>;
+    for (let i = 0; i < requiredFields.length; i++) {
+      if (!requiredFields[i].value) {
+        alert('Por favor, rellena todos los campos requeridos.');
+        return false;
+      }
+    }
+    this.confirmOrder();
+    return true;
+  }
+
+  confirmOrder(){
+    this.cartService.doCheckout().subscribe(() => {
+        //Mandar a la página user
+      });;
+  }
+
+}
