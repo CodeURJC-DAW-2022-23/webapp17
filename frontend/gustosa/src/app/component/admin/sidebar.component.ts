73d7bd0bce34745204abba421be12059f76877df
rattata2me <jorgevegarias1@gmail.com>
2023-04-24 14:42:20 +0200

    Admin checkout

--- frontend/gustosa/src/app/component/admin/sidebar.component.ts
+++ frontend/gustosa/src/app/component/admin/sidebar.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { SessionService } from 'src/app/service/session.service';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'sidebar',
@@ -6,5 +10,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./general-admin-style.css'],
 })
 export class SidebarComponent {
-
+  constructor(private router: Router, private userService:UserService, 
+    private sessionService : SessionService, private modalService: BsModalService){
+    userService.isUserLoggedIn().subscribe((val)=>{
+        if(!val){
+            sessionService.updateProfile();
+            router.navigateByUrl("login");
+        }
+    });
+  }
 }
