fd33641467bf003a5000ba4bebc0236872b50e1f
rattata2me <jorgevegarias1@gmail.com>
2023-04-24 14:37:58 +0200

    SQL errors fixup

--- backend/src/main/java/es/codeurjc/webapp17/repository/UsersRepo.java
+++ backend/src/main/java/es/codeurjc/webapp17/repository/UsersRepo.java
@@ -12,12 +12,12 @@ public interface UsersRepo extends JpaRepository<UserProfile, Long>{
 
 
     // This query computes the near path recomendations for products
-    @Query(value="select product_id from (select created_by_id, product_id from cart join cart_item on cart_item.cart_id=cart.id) "+
-    "join (select created_by_id as c, product_id as p from cart join cart_item on cart_item.cart_id=cart.id) where "+
-    "(p in (select product_id from cart join cart_item on cart_item.cart_id=cart.id where cart_id=:order) and c=created_by_id) group"+
-    " by product_id order by count(product_id) desc", nativeQuery = true)
+    @Query(value="select product_id from (select created_by_id, product_id from cart join cart_item on cart_item.cart_id=cart.id) as t "+
+    "join (select created_by_id as c, product_id as p from cart join cart_item on cart_item.cart_id=cart.id) as q where "+
+    "(p in (select product_id from cart join cart_item on cart_item.cart_id=cart.id where cart_id=:order) and c=created_by_id) group "+
+    " by product_id order by count(product_id) desc;", nativeQuery = true)
     List<Long> getRecomendedByProductList(@Param("order") Long order);
 
-    @Query(value="SELECT COUNT(*) AS total_users FROM USER_PROFILE;", nativeQuery = true)
+    @Query(value="select count(*) as total_users from user_profile", nativeQuery = true)
     int getTotalUsers();
 }
