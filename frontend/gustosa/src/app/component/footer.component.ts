356e46e151eb0f17e307522a2deac55ff3c48fcb
Ggronda <jesus@debian-BULLSEYE-live-builder-AMD64>
2023-04-24 18:51:32 +0200

    Footer Component Html

---
+++ frontend/gustosa/src/app/component/footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Observable, Subject, catchError, map, of, retry } from 'rxjs';
+
+@Component({
+  selector: 'footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+
+export class FooterComponent {
+ 
+}
